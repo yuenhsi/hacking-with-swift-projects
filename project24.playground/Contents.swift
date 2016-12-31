234d230ff1463b31f3c77e6706b8ce2cc7a84cb8
Yuen Hsi Chang <yuenhsi93@gmail.com>
2016-12-30 16:47:01 -0800

    play with swift extensions

--- project24.playground/Contents.swift
+++ project24.playground/Contents.swift
@@ -2,4 +2,41 @@
 
 import UIKit
 
-var str = "Hello, playground"
+extension Int {
+    func plusOne() -> Int {
+        return self + 1
+    }
+}
+
+extension Int {
+    mutating func plusOneMutate() {
+        self += 1
+    }
+}
+
+var myInt = 0
+myInt.plusOne()
+myInt
+myInt.plusOneMutate()
+myInt
+
+extension Int {
+    func squared() -> Int {
+        return self * self
+    }
+}
+
+let i: Int = 8
+print(i.squared())
+
+let j: UInt64 = 8
+//print(j.squared())
+
+extension Integer {
+    func squaredDataType() -> Self {
+        return self * self
+    }
+}
+
+let k: UInt64 = 8
+print(k.squaredDataType())
